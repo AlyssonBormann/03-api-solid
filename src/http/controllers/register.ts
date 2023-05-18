07068ace493633ec50cdac167b887cfcae0986a7
Alysson Bormannn <alysson.bormannn@gmail.com>
2023-05-18 08:24:24 -0400

    :sparkles: feat: create controller authenticate

--- src/http/controllers/register.ts
+++ src/http/controllers/register.ts
@@ -17,7 +17,7 @@ export async function register(request: FastifyRequest, reply: FastifyReply) {
     const prismaUsersRepository = new PrismaUsersRepository()
     const registerUseCase = new RegisterUseCase(prismaUsersRepository)
 
-    await registerUseCase.execulter({ name, email, password })
+    await registerUseCase.execute({ name, email, password })
   } catch (err) {
     if (err instanceof UserAlreadyExistsError) {
       return reply.status(409).send({ message: err.message })
